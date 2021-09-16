66be2f391fb60012a152fce2c439d4cd65228392
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-09-15 20:40:32 -0400

    init

---
+++ documentation/api.js
@@ -0,0 +1,41 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Add",
+        "Dann",
+        "Layer",
+        "Matrix"
+    ],
+    "modules": [
+        "Create",
+        "Dann",
+        "Interact",
+        "Share",
+        "Train"
+    ],
+    "allModules": [
+        {
+            "displayName": "Create",
+            "name": "Create",
+            "description": "Deep Neural Network object. Can be trained with data or by neuro-evolution."
+        },
+        {
+            "displayName": "Dann",
+            "name": "Dann"
+        },
+        {
+            "displayName": "Interact",
+            "name": "Interact"
+        },
+        {
+            "displayName": "Share",
+            "name": "Share"
+        },
+        {
+            "displayName": "Train",
+            "name": "Train"
+        }
+    ],
+    "elements": []
+} };
+});
